d04d1c8192150da7e6a261fc436889688cdc962b
Joe <Walktheworld@me.com>
2021-06-22 18:49:17 -0600

    6/22 edits to add Form.js

---
+++ my-app/src/form.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Form extends Component{
+    render (){
+        return (
+            <form className= "form">
+                <input></input>
+                <input></input>
+                <button>HIT ME!!!</button>
+            </form>
+        )
+    }
+
+}
+
+export default Form;
